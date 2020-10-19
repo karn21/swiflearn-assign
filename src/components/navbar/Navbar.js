958580cdc2b7f6424578ad53df3cb6c6b5c75af7
karn21 <karn212000@gmail.com>
2020-10-19 18:40:22 +0530

    make page responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,23 +2,32 @@ import React, { Component } from "react";
 import "./navbar.css";
 
 class Navbar extends Component {
+  state = {
+    showNav: false,
+  };
+
+  // Function to toggle navbar visibility
+  toggleNav = () => {
+    this.setState({
+      showNav: !this.state.showNav,
+    });
+  };
+
   render() {
     return (
-      <nav>
-        <ul className="nav-links">
-          <li className="nav-link">
-            <a href="#">Live Consultation</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Buy Plans</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">View Plans</a>
-          </li>
+      <nav className={this.state.showNav ? "show" : ""}>
+        <ul className={this.state.showNav ? `show nav-links` : `nav-links`}>
+          <li className="nav-link">Live Consultation</li>
+          <li className="nav-link">Buy Plans</li>
+          <li className="nav-link">View Plans</li>
           <li className="nav-link">
             <button className="login-btn">Log In</button>
           </li>
         </ul>
+
+        <div className="menu" onClick={this.toggleNav}>
+          <i class="fa fa-bars" aria-hidden="true"></i> Menu
+        </div>
       </nav>
     );
   }
